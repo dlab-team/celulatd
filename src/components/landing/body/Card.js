bc7a210202cc62d2f56f35a730040ec54258f3fd
Edix77 <edinavast@gmail.com>
2023-03-24 15:24:53 -0300

    Actualizacion Landing mobile
    Se da altura a las distintas partes de toda la estructura modificando la grid, especificamente el alto de las Row, se mejoran las card en version mobile.

--- src/components/landing/body/Card.js
+++ src/components/landing/body/Card.js
@@ -19,92 +19,157 @@ import '../../../scss/components/landing/body/_card.scss';
 export default function Card() {
   return (
     <>
-        <div className='main__card1-mobile'>
-          <div className='main__card1-mobile--content'>
-            <img className='main__card1-mobile--svg' src={menorque}
-            />
-            <img className='main__card1-mobile--img' src={card1} />
-            <p className='main__card1-mobile--p'>
-              Lorem ipsum dolor sit amet consectetur. Sed odio
+      <div className='main__card1-mobile'>
+        <div className='main__card1-mobile--content'>
+          <img className='main__card1-mobile--svg' src={menorque}
+          />
+          <img className='main__card1-mobile--img' src={card1} />
 
-              egestas tincidunt mauris mollis senectus tellus
+          <p className='main__card1-mobile--p'>
+            Lorem ipsum dolor sit amet consectetur. Sed odio
 
-              Condimentum egestas dignissim venenatis magna
+            egestas tincidunt mauris mollis senectus tellus
 
-              ut at a id placerat. Donec gravida elit velit quisque.
+            Condimentum egestas dignissim venenatis magna
 
-            </p>
-          </div>
+            ut at a id placerat. Donec gravida elit velit quisque.
+            Lorem ipsum dolor sit amet consectetur. Sed odio
+
+            egestas tincidunt mauris mollis senectus tellus
+
+            Condimentum egestas dignissim venenatis magna
+
+            ut at a id placerat. Donec gravida elit velit quisque.
+            Lorem ipsum dolor sit amet consectetur. Sed odio
+
+            egestas tincidunt mauris mollis senectus tellus
+
+            Condimentum egestas dignissim venenatis magna
+
+            ut at a id placerat. Donec gravida elit velit quisque.
+            Lorem ipsum dolor sit amet consectetur. Sed odio
+
+            egestas tincidunt mauris mollis senectus tellus
+
+            Condimentum egestas dignissim venenatis magna
+
+            ut at a id placerat. Donec gravida elit velit quisque.
+          </p>
+
+
         </div>
+      </div>
 
-        <div className='main__card1-desktop'>
-          <div className='main__card1-desktop--content'>
-            <img className='main__card1-desktop--svg' src={menorque} />
-            <img className='main__card1-desktop--img'></img>
-            <p className='main__card1-desktop--p'>
-              texto texto texto texto texto texto texto textotextotextotextotextotextotexto
-              texto texto texto texto texto texto texto textotextotextotextotextotextotexto
+      <div className='main__card1-desktop'>
+        <div className='main__card1-desktop--content'>
+          <img className='main__card1-desktop--svg' src={menorque} />
+          <img className='main__card1-desktop--img'></img>
+          <p className='main__card1-desktop--p'>
+            texto texto texto texto texto texto texto textotextotextotextotextotextotexto
+            texto texto texto texto texto texto texto textotextotextotextotextotextotexto
 
-            </p>
-          </div>
+          </p>
         </div>
+      </div>
 
-        <div className='main__card2-mobile'>
-                    <div className='main__card2-mobile--content'>
-                        <img className='main__card2-mobile--svg' src={slash} />
-                        <img className='main__card2-mobile--img' src={card2} />
-                        <p className='main__card2-mobile--p'>
-                            Lorem ipsum dolor sit amet consectetur. Sed odio
+      <div className='main__card2-mobile'>
+        <div className='main__card2-mobile--content'>
+          <img className='main__card2-mobile--svg' src={slash} />
+          <img className='main__card2-mobile--img' src={card2} />
+          <p className='main__card2-mobile--p'>
+            Lorem ipsum dolor sit amet consectetur. Sed odio
 
-                            egestas tincidunt mauris mollis senectus tellus
+            egestas tincidunt mauris mollis senectus tellus
 
-                            Condimentum egestas dignissim venenatis magna
+            Condimentum egestas dignissim venenatis magna
 
-                            ut at a id placerat. Donec gravida elit velit quisque.
+            ut at a id placerat. Donec gravida elit velit quisque.
+            Lorem ipsum dolor sit amet consectetur. Sed odio
 
-                        </p>
-                    </div>
-                </div>
+            egestas tincidunt mauris mollis senectus tellus
 
-                <div className='main__card2-desktop'>
-                    <div className='main__card2-desktop--content'>
-                        <img className='main__card2-desktop--svg'></img>
-                        <img className='main__card2-desktop--img'></img>
-                        <p className='main__card2-desktop--p'>
-                            texto texto texto texto texto texto texto textotextotextotextotextotextotexto
-                            texto texto texto texto texto texto texto textotextotextotextotextotextotexto
+            Condimentum egestas dignissim venenatis magna
 
-                        </p>
-                    </div>
-                </div>
+            ut at a id placerat. Donec gravida elit velit quisque.
+            Lorem ipsum dolor sit amet consectetur. Sed odio
 
-                <div className='main__card3-mobile'>
-                    <div className='main__card3-mobile--content'>
-                        <img className='main__card3-mobile--svg' src={mayorque} />
-                        <img className='main__card3-mobile--img' src={card3}/>
-                        <p className='main__card3-mobile--p'>
-                            Lorem ipsum dolor sit amet consectetur. Sed odio
+            egestas tincidunt mauris mollis senectus tellus
 
-                            egestas tincidunt mauris mollis senectus tellus
+            Condimentum egestas dignissim venenatis magna
 
-                            Condimentum egestas dignissim venenatis magna
+            ut at a id placerat. Donec gravida elit velit quisque.
+            Lorem ipsum dolor sit amet consectetur. Sed odio
 
-                            ut at a id placerat. Donec gravida elit velit quisque.
-                        </p>
-                    </div>
-                </div>
+            egestas tincidunt mauris mollis senectus tellus
 
-                <div className='main__card3-desktop'>
-                    <div className='main__card3-desktop--content'>
-                        <img className='main__card3-desktop--svg'></img>
-                        <img className='main__card3-desktop--img'></img>
-                        <p className='main__card3-desktop--p'>
-                            texto texto texto texto texto texto texto textotextotextotextotextotextotexto
-                            texto texto texto texto texto texto texto textotextotextotextotextotextotexto
-                        </p>
-                    </div> 
-                </div>
-      
+            Condimentum egestas dignissim venenatis magna
+
+            ut at a id placerat. Donec gravida elit velit quisque.
+
+          </p>
+        </div>
+      </div>
+
+      <div className='main__card2-desktop'>
+        <div className='main__card2-desktop--content'>
+          <img className='main__card2-desktop--svg'></img>
+          <img className='main__card2-desktop--img'></img>
+          <p className='main__card2-desktop--p'>
+            texto texto texto texto texto texto texto textotextotextotextotextotextotexto
+            texto texto texto texto texto texto texto textotextotextotextotextotextotexto
+
+          </p>
+        </div>
+      </div>
+
+      <div className='main__card3-mobile'>
+        <div className='main__card3-mobile--content'>
+          <img className='main__card3-mobile--svg' src={mayorque} />
+          <img className='main__card3-mobile--img' src={card3} />
+          <p className='main__card3-mobile--p'>
+            Lorem ipsum dolor sit amet consectetur. Sed odio
+
+            egestas tincidunt mauris mollis senectus tellus
+
+            Condimentum egestas dignissim venenatis magna
+
+            ut at a id placerat. Donec gravida elit velit quisque.
+            Lorem ipsum dolor sit amet consectetur. Sed odio
+
+            egestas tincidunt mauris mollis senectus tellus
+
+            Condimentum egestas dignissim venenatis magna
+
+            ut at a id placerat. Donec gravida elit velit quisque.
+            Lorem ipsum dolor sit amet consectetur. Sed odio
+
+            egestas tincidunt mauris mollis senectus tellus
+
+            Condimentum egestas dignissim venenatis magna
+
+            ut at a id placerat. Donec gravida elit velit quisque.
+            Lorem ipsum dolor sit amet consectetur. Sed odio
+
+            egestas tincidunt mauris mollis senectus tellus
+
+            Condimentum egestas dignissim venenatis magna
+
+            ut at a id placerat. Donec gravida elit velit quisque.
+          </p>
+        </div>
+      </div>
+
+      <div className='main__card3-desktop'>
+        <div className='main__card3-desktop--content'>
+          <img className='main__card3-desktop--svg'></img>
+          <img className='main__card3-desktop--img'></img>
+          <p className='main__card3-desktop--p'>
+            texto texto texto texto texto texto texto textotextotextotextotextotextotexto
+            texto texto texto texto texto texto texto textotextotextotextotextotextotexto
+          </p>
+        </div>
+      </div>
+
     </>
   )
 }

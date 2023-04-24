b60776dc1808a5ddbadc425e3c46aeee7aa8638c
ISTEMEFA <israelmege@gmail.com>
2023-04-24 08:44:10 -0400

    se arreglean componentes de commos

--- src/components/common/Error404.jsx
+++ src/components/common/Error404.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import "../../../src/scssWeb/main.css";
-import Movil404 from '../../assets/img/movil404.svg'
-// import Dktp404 from '../../assets/img/dktp404.svg'
+import Movil404 from "../../assets/img/movil404.svg";
+ //import Dktp404 from '../../assets/img/dktp404.svg'
 
+export const Error404 = () => {
+  return (
+    <div className="container-error">
+      <div className="container-error-movil">
+        <img src={Movil404} alt="pagina no encontrada" />
+      </div>
 
-const Error404 = () => {
-return (
-<div className="container-error">
-<div className="container-error-movil">
-<img src={Movil404} alt="pagina no encontrada" />
-</div>
-
-{/* <div className="container-error-desktop">
+      {/* <div className="container-error-desktop">
 <img src={Dktp404} />
 </div> */}
-</div>
-);
+    </div>
+  );
 };
-export default Error404;
--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -3,12 +3,11 @@ import "../../scssWeb/main.css";
 import LogoSvg from "../../assets/img/LogoVector.svg";
 import FraseSvg from "../../assets/img/Frase1Vector.svg";
 
-function Header({ userName }) {
+export default function Header({ userName }) {
   const currentDate = new Date().toLocaleDateString();
   const avatar =
     "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50";
 
-    
   return (
     <header className="header-container">
       <div className="header-container-logo">
@@ -20,10 +19,6 @@ function Header({ userName }) {
         <p className="welcome-message">Bienvenido,</p>
         <p className="user-name">{userName}</p>
       </div>
-      {/* <div className="search-container">
-        <input type="text" placeholder="Buscar" className="search-input" />
-        <button className="search-button">Buscar</button>
-      </div> */}
       <div className="settings-container">
         <div className="date-container">{currentDate}</div>
         <button className="alarm-button">Alarma</button>
@@ -33,4 +28,4 @@ function Header({ userName }) {
   );
 }
 
-export default Header;
+

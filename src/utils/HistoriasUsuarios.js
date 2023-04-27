b523b86fb773089c6c97c9b5832160587a94544a
ISTEMEFA <israelmege@gmail.com>
2023-04-27 08:46:16 -0400

    se agrega img a carrusel historias

--- src/utils/HistoriasUsuarios.js
+++ src/utils/HistoriasUsuarios.js
@@ -1,16 +1,23 @@
+import foto from "../assets/img/Quincy-Larson-photo.jpg"
+
 const HistoriaUsuarios = [
-    {
-    "nombre": "SEBATIÁN QUEVEDO.",
-    "historia": "Resalto que de acá salen muy buenos elementos y que me han salido muchas ofertas laborales."
-    },
-    {
-      "nombre": " TRINIDAD SWINBURN",
-      "historia": "La Academia Desafío Latam y toda su comunidad,me insertaron en un camino profesional muy dinámico y activo."
-    },
-    {
-      "nombre": " SERGIO DELGADO",
-      "historia": "Es una experiencia bonita, el compañerismo es super buenoy en poco tiempo pudimos lograr muchas cosas.."
-    },
-    
-  ]
-  export default HistoriaUsuarios;
+  {
+    rutaimg: {foto},
+    nombre: "SEBATIÁN QUEVEDO.",
+    historia:
+      "Resalto que de acá salen muy buenos elementos y que me han salido muchas ofertas laborales.",
+  },
+  {
+    rutaimg: {foto},
+    nombre: " TRINIDAD SWINBURN",
+    historia:
+      "La Academia Desafío Latam y toda su comunidad,me insertaron en un camino profesional muy dinámico y activo.",
+  },
+  {
+    rutaimg: {foto},
+    nombre: " SERGIO DELGADO",
+    historia:
+      "Es una experiencia bonita, el compañerismo es super buenoy en poco tiempo pudimos lograr muchas cosas..",
+  },
+];
+export default HistoriaUsuarios;
